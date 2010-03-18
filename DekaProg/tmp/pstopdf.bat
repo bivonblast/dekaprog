c0a41530232b7a8810930e7d80a662a49f91c85f
nils.martin.andersson@gmail.com <nils.martin.andersson@gmail.com@ecf91e86-09c1-11df-b317-0f3410dc57f6>
2010-03-18 19:16:09 +0000

    PDF hantering inkopplad och fungerande. Avvaktar klassen Character.

---
+++ DekaProg/tmp/pstopdf.bat
@@ -0,0 +1,6 @@
+set PATH=%PATH%;c:\Program\gs\gs8.64\lib
+set PATH=%PATH%;c:\Program\gs\gs8.64\bin
+
+c:\Program\gs\gs8.64\lib\ps2pdf.bat C:\DekaProg\test.ps %1
+
+#c:\Program\gs\gs8.64\lib\ps2pdf.bat C:\DekaProg\rollf.eps %1
