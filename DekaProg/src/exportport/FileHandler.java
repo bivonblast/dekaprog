a5659c187e1dca714461f320d710eb8630fb5aff
nils.martin.andersson@gmail.com <nils.martin.andersson@gmail.com@ecf91e86-09c1-11df-b317-0f3410dc57f6>
2010-03-19 22:37:08 +0000

    Obetydlig uppdatering

---
+++ DekaProg/src/exportport/FileHandler.java
@@ -0,0 +1,89 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package exportport;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * An ExportImportHandler that sends the data to files.
+ *
+ * @version 0.6
+ * @author Martin Andersson
+ * @see ExportImport.ExportImportHandler
+ * @see ExportImport.WikiHandler
+ */
+public class FileHandler extends ExportImportHandler{
+    private File currentFile;
+    private BufferedReader currentFileReader = null;
+    private BufferedWriter currentFileWriter;
+    private String character;
+
+    public FileHandler(){
+        super("file:///C:/Users/Martin/AppData/Roaming/DekaProg/");  //Ska vara %appdata%\DekaProg
+    }
+
+    public FileHandler(String location){
+        super(location);
+    }
+
+    @Override
+    public String readCharacter(String characterName) {
+        try {
+            currentFile = new File(new URI(location + characterName.replaceAll(" ", "%20") + ".dkp"));
+            currentFileReader = new BufferedReader(new FileReader(currentFile));
+            character = currentFileReader.readLine();
+            String tmpLine;
+            while (( tmpLine = currentFileReader.readLine()) != null) {
+                character = character + "\n" + tmpLine;
+            }
+            currentFileReader.close();
+            return character;
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(FileHandler.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(FileHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    @Override
+    public String readSettings(String campaignName) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean writeCharacter(String characterName, String character) {
+        try {
+            currentFile = new File(new URI(location + characterName.replaceAll(" ", "%20") + ".dkp"));
+            currentFileWriter = new BufferedWriter(new FileWriter(currentFile));
+
+            currentFileWriter.write(character);
+            currentFileWriter.flush();
+            currentFileWriter.close();
+            return true;
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(FileHandler.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(FileHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean writeSettings(String campaignName, String campaign) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
---
+++ DekaProg/src/exportport/WikiHandler.java
@@ -0,0 +1,130 @@
+package exportport;
+
+import java.net.MalformedURLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import net.sourceforge.jwbf.actions.mediawiki.editing.PostDelete;
+import net.sourceforge.jwbf.actions.mediawiki.editing.PostModifyContent;
+import net.sourceforge.jwbf.actions.mediawiki.meta.Siteinfo;
+import net.sourceforge.jwbf.actions.util.ActionException;
+import net.sourceforge.jwbf.actions.util.ProcessException;
+import net.sourceforge.jwbf.bots.MediaWikiBot;
+import net.sourceforge.jwbf.contentRep.Article;
+import net.sourceforge.jwbf.contentRep.Userinfo;
+
+
+
+/**
+ * An ExportImportHandler that sends the data to a mediaWiki using jwbf bot program.
+ *
+ * @version 0.5A
+ * @author Martin Andersson
+ * @see ExportImport.ExportImportHandler
+ * @see ExportImport.FileHandler
+*/
+public class WikiHandler extends ExportImportHandler{
+    private MediaWikiBot wikiConnection;    //Kopplingen till mediaWikin
+    private String username;
+    private String password;
+    private Article curArticle;
+
+    public WikiHandler(String location){
+        super(location);
+        if (connect()){
+            this.username = "";
+            this.password = "";
+        }
+    }
+
+    public WikiHandler(String location, String username, String password){
+        super(location);
+        if (connect(username, password)){
+            this.username = username;
+            this.password = password;
+        }
+    }
+
+    /**
+     * Login as a user with no write privilieges.
+     * NOTE: The user DekaProg has write privilieges at the moment to simplify testing of
+     * writing to wiki-part.
+     * @return Returns true if login was successful, false otherwise.
+     */
+    public boolean connect() {
+        return connect("DekaProg", "tester");
+    }
+
+    /**
+     * Login as a user with username and password.
+     * @param username The users username
+     * @param password The users password
+     * @return Returns true if login was successful, false otherwise.
+     */
+    public boolean connect(String username, String password) {
+        try {
+            wikiConnection = new MediaWikiBot("http://wiki.raspare.se/");//new URL(location));
+            wikiConnection.login(username, password);
+            return true;
+        } catch (ActionException ex) {
+            Logger.getLogger(WikiHandler.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (MalformedURLException ex) { 
+            Logger.getLogger(WikiHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+
+    @Override
+    public String readCharacter(String characterName) {
+        try {
+            curArticle = wikiConnection.readContent(characterName);
+
+            return curArticle.getText().toString();
+        } catch (ActionException ex) {
+            Logger.getLogger(WikiHandler.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ProcessException ex) {
+            Logger.getLogger(WikiHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    @Override
+    public boolean writeCharacter(String characterName, String character) {
+        try {
+            curArticle.setText(character);
+            //wikiConnection.performAction(new PostModifyContent(wikiConnection, curArticle));
+            wikiConnection.writeContent(curArticle);
+            return true;
+        } catch (ActionException ex) {
+            Logger.getLogger(WikiHandler.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ProcessException ex) {
+            Logger.getLogger(WikiHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+
+    @Override
+    public String readSettings(String campaignName) {
+        try {
+            curArticle = wikiConnection.readContent(campaignName);
+
+            return curArticle.getText().toString();
+        } catch (ActionException ex) {
+            Logger.getLogger(WikiHandler.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ProcessException ex) {
+            Logger.getLogger(WikiHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    @Override
+    public boolean writeSettings(String campaignName, String campaign) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public String[] parseWikiCategories(Article article){
+        //for(int i = 0 ; i < character.antalfärdigheter() ; i++)
+        //gå igenom de sata färdigheterna hos karaktären och kolla om det finns i WikiArtikeln
+        //
+        return null;
+    }
+}
