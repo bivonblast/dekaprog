30ea9a94f85ebbc80670abdc7b99c044d78a6ba2
frjo02 <frjo02@ecf91e86-09c1-11df-b317-0f3410dc57f6>
2010-03-26 17:16:19 +0000

    Forgot to update DekaederCharacter. Fixed now.

--- DekaProg/src/character/DekaederCharacter.java
+++ DekaProg/src/character/DekaederCharacter.java
@@ -4,19 +4,33 @@ import character.data.*;
 
 public class DekaederCharacter {
 	//variabler
-	private ConceptHandler conceptHandler;
-	private PointHandler	pointHandler;
+	private ConceptHandler      conceptHandler;
+	private PointHandler        pointHandler;
+        private SkillHandler        skillHandler;
+        private SpecialityHandler   specialityHandler;
 
-	public DekaederCharacter(ConceptHandler conceptHandler, PointHandler pointHandler) {
-		this.conceptHandler = conceptHandler;
-		this.pointHandler = pointHandler;
+	public DekaederCharacter(ConceptHandler conceptHandler, 
+            PointHandler pointHandler, SkillHandler skillHandler,
+            SpecialityHandler specialityHandler) {
+            this.conceptHandler = conceptHandler;
+            this.pointHandler = pointHandler;
+            this.skillHandler = skillHandler;
+            this.specialityHandler = specialityHandler;
 	}
 
 	public ConceptHandler getConceptHandler() {
-		return conceptHandler;
+            return conceptHandler;
 	}
 
 	public PointHandler getPointHandler() {
-		return pointHandler;
+            return pointHandler;
 	}
+
+        public SkillHandler getSkillHandler() {
+            return skillHandler;
+        }
+
+        public SpecialityHandler getSpecialityHandler() {
+            return specialityHandler;
+        }
 }
