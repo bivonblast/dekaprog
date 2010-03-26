da048bdd96a6e97286f72580ff641ec25c546752
nils.martin.andersson@gmail.com <nils.martin.andersson@gmail.com@ecf91e86-09c1-11df-b317-0f3410dc57f6>
2010-03-26 07:11:30 +0000

    En uppdatering i PDF-hanteraren. Hanterar nu points och concept...

--- DekaProg/test/TestImport.java
+++ DekaProg/test/TestImport.java
@@ -16,14 +16,20 @@ public class TestImport {
     public static void main(String[] args) {
         ExportHandler pdfTest = new PDFHandler("file:///C:/DekaProg/",
                 45, 532, 11.3f, 87, 114f);
-        ConceptHandler conceptHandler = new ConceptHandler("Levan Darish", 
-                "Karisma",
-                "Upprorets Piloter",
-                ConceptHandler.GENDER_MALE,
-                21,
-                "Cool kille",
-                "One down! Two to go...");
-        PointHandler pointHandler = new PointHandler(50, 10, 0, 165+50, 29, 2);
+        ConceptHandler conceptHandler = new ConceptHandler();
+        conceptHandler.addConceptTrait("Namn", "Levan Darish");
+        conceptHandler.addConceptTrait("Kön", "Man");
+        conceptHandler.addConceptTrait("Ålder", "21 år");
+        conceptHandler.addConceptTrait("Koncept", "Karisma");
+        conceptHandler.addConceptTrait("Kampanj", "Upprorets Piloter");
+        conceptHandler.addConceptTrait("Utseende", "Cool kille");
+        conceptHandler.addConceptTrait("Citat", "One down! Two to go...");
+        PointHandler pointHandler = new PointHandler(50);
+        pointHandler.addPointTrait("Karma", 10);
+        pointHandler.addPointTrait("Stilpoäng", 2);
+        pointHandler.addPointTrait("VeteranÄventyr", 29);
+        pointHandler.addExperiencePoints(169);
+        pointHandler.spendExperiencePoints(219, 0);
         DekaederCharacter test = new DekaederCharacter(conceptHandler, pointHandler);
         pdfTest.writeCharacter(test, "Myggdräpe");
     }
