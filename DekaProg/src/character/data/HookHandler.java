9816b57bcf9f4dbd3fe16f765b00624badbf5154
frjo02 <frjo02@ecf91e86-09c1-11df-b317-0f3410dc57f6>
2010-04-02 10:26:51 +0000

    Changed the way Hooks work.

--- DekaProg/src/character/data/HookHandler.java
+++ DekaProg/src/character/data/HookHandler.java
@@ -15,72 +15,34 @@ public class HookHandler extends TraitHandler<HookTrait> {
 
     public HookHandler() {
         super();
+        add(new HookTrait("Drivkraft"));
+        add(new HookTrait("Uppträdande"));
+        add(new HookTrait("Svaghet"));
     }
 
-   public boolean addHookTrait(String hookName, HookType hookType) {
-        HookTrait hook = new HookTrait(hookName, hookType);
+    public HookHandler(String incentiveValue, String behaviourValue,
+            String weaknessValue) {
+        super();
+        add(new HookTrait("Drivkraft", incentiveValue));
+        add(new HookTrait("Uppträdande", behaviourValue));
+        add(new HookTrait("Svaghet", weaknessValue));
+    }
+
+    public void setIncetiveValue(String incentiveValue) {
         for (HookTrait ht : this) {
-            if (ht.equals(hook)) return false;
+            if (ht.getName().equals("Drivkraft")) ht.setValue(incentiveValue);
         }
-        add(hook);
-        return true;
     }
 
-    public boolean addHookTrait(HookTrait hook) {
+    public void setBehaviourValue(String behaviourValue) {
         for (HookTrait ht : this) {
-            if (ht.equals(hook)) return false;
+            if (ht.getName().equals("Uppträdande")) ht.setValue(behaviourValue);
         }
-        add(hook);
-        return true;
     }
 
-
-    /**
-     * Removes the {@code PointTrait} with the specified name from the
-     * {@code PointHandler}, given that it exists.
-     * @param traitName The name of the {@code PointTrait} to be removed.
-     * @return {@code true} if the {@code PointTrait} was succesfully removed,
-     * {@code false} if it didn't exist.
-     */
-        public boolean removeHookTrait(String traitName) {
-            for (int i = 0; i < size(); i++) {
-                if (get(i).getName().equalsIgnoreCase(traitName)) {
-                    remove(i);
-                    return true;
-                }
-            }
-            return false;
+    public void setWeaknessalue(String weaknessValue) {
+        for (HookTrait ht : this) {
+            if (ht.getName().equals("Svaghet")) ht.setValue(weaknessValue);
         }
-
-        public HookTrait getHook(String traitName) {
-            for (HookTrait ht : this) {
-                if (ht.getName().equalsIgnoreCase(traitName)) return ht;
-            }
-            return null;
-        }
-
-        public Vector<HookTrait> getIncentiveHooks() {
-            Vector<HookTrait> ret = new Vector<HookTrait>();
-            for (HookTrait ht : this) {
-                if (ht.isIncentive()) ret.add(ht);
-            }
-            return ret;
-        }
-
-        public Vector<HookTrait> getAppearanceHooks() {
-            Vector<HookTrait> ret = new Vector<HookTrait>();
-            for (HookTrait ht : this) {
-                if (ht.isAppearance()) ret.add(ht);
-            }
-            return ret;
-        }
-
-        public Vector<HookTrait> getWeaknessHooks() {
-            Vector<HookTrait> ret = new Vector<HookTrait>();
-            for (HookTrait ht : this) {
-                if (ht.isWeakness()) ret.add(ht);
-            }
-            return ret;
-        }
-
+    }
 }
